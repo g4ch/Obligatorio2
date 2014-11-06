0f2c70d227ffe0a57828be197b74316ae7bbbff1
germalvarez <germalvarez@gmail.com>
2014-11-06 18:31:37 -0200

    Clase persona con contructor y metodos basicos

--- Obligatorio2/src/obligatorio2/Persona.java
+++ Obligatorio2/src/obligatorio2/Persona.java
@@ -6,10 +6,80 @@
 
 package obligatorio2;
 
+import java.util.Date;
+
 /**
  *
  * @author alumnoFI
  */
 public class Persona {
+    String cedula="1111111-1";
+    String nombre="Pedro";
+    String apellido="Gonzalez";
+    String direccion="Cuareim 1451";
+    Date nacimiento=new Date();
+    String email="pgonzalez@gmail.com";
+    String telefono="094123456";
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return cedula + "|" + nombre + "|" + apellido + "|" + direccion + "|" + nacimiento + "|" + email + "|" + telefono + "|";
+    }
     
 }
